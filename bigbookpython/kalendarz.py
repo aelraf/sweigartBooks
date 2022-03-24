8c2d244e0f0c3bbd96e0461b9e3087625e409f9e
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-24 19:16:21 +0100

    kalendarz - metoda get_year_from_user()

--- bigbookpython/kalendarz.py
+++ bigbookpython/kalendarz.py
@@ -14,7 +14,24 @@ import datetime
 
 
 DAYS = ('Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota')
-MONTHS = ()
+MONTHS = ('Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec',
+          'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień')
+
+
+def get_year_from_user() -> int:
+    year = 0
+    while True:
+        print('Podaj rok dla kalendarza: ')
+        response = input('> ')
+
+        if response.isdecimal() and int(response) > 0:
+            year = int(response)
+            break
+
+        print('Proszę podać wartość liczbową, w stylu 2022.')
+        continue
+
+    return year
 
 
 def get_calendar_for(year, month):
