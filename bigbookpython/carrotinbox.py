160c7b8778c8c22b1c2210ffbc35ee3f3ecb8392
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-25 22:32:36 +0100

    carrotinbox - opis

---
+++ bigbookpython/carrotinbox.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# RafKac
+
+"""
+Gra dla dwóch osób - marchew w pudełku.
+Każdy gracz ma pudełko. W jednym z nich jest marchew, każdy gracz chce ją mieć.
+Pierwszy gracz zagląda do swojego pudła i informuje drugiego, czy ma marchew.
+
+Drugi gracz decyduje, czy zamienić pudła, czy nie.
+"""
+
