297ca3e66919786582dcaf7c75e240ccf0a7b79e
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-30 21:36:51 +0200

    ciąg collatza - metoda collatz_sequence

--- bigbookpython/collatzsequence.py
+++ bigbookpython/collatzsequence.py
@@ -14,7 +14,7 @@ import sys
 import time
 
 
-def get_n_from_user():
+def get_n_from_user() -> int:
     while True:
         print("Podaj wartość początkową większą od 0 lub zakończ (QUIT):")
         response = input('> ')
@@ -27,9 +27,23 @@ def get_n_from_user():
             print('Wartość n musi być większa od 0, podaj jeszcze raz.')
             continue
 
+        if int(response) > 1:
+            return int(response)
 
 
+def collatz_sequence(n: int):
+    print(n, end='', flush=True)
 
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+
+        print(', ' + str(n), end='', flush=True)
+        time.sleep(0.1)
+
+    print()
 
 
 def main():
