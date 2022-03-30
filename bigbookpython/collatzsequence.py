09fc79c5829ff3e96f220027b8fa1c1c7b7cf4bf
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-30 21:15:11 +0200

    ciąg collatza - komentarze z opisem algorytmu

---
+++ bigbookpython/collatzsequence.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# RafKac
+
+"""
+Ciąg Collatza:
+- dla n parzystego następnikiem jest n/2
+- dla n nieparzystego, następnikiem jest n*3 + 1
+- dla n=1 kończymy działanie.
+
+User podaje liczbę, program sprawdza, czy wyjdzie 1.
+"""
+
+
