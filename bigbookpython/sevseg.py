55c87774b44d9f6f8ae8102a6cc2d4118962b39c
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-03-31 18:18:27 +0200

    get_sev_seg_str - puste, 0, 1, i 2

--- bigbookpython/sevseg.py
+++ bigbookpython/sevseg.py
@@ -18,6 +18,31 @@ E     C
 def get_sev_seg_str(number, min_width=0):
     number = str(number).zfill(min_width)
 
+    rows = ['', '', '']
+
+    for i, numeral in enumerate(number):
+        if numeral == ".":
+            rows[0] += ' '
+            rows[1] += " "
+            rows[2] += "."
+            continue
+        elif numeral == '-':
+            rows[0] += '    '
+            rows[1] += " __ "
+            rows[2] += "    "
+        elif numeral == "0":
+            rows[0] += ' __ '
+            rows[1] += "|  |"
+            rows[2] += "|__|"
+        elif numeral == "1":
+            rows[0] += '    '
+            rows[1] += "   |"
+            rows[2] += "   |"
+        elif numeral == "2":
+            rows[0] += ' __ '
+            rows[1] += " __|"
+            rows[2] += "|__ "
+
 
 if __name__ == "__main__":
     print("Ten moduł jest do importowania do innych zastosowań, a nie uruchamiania stricte")
