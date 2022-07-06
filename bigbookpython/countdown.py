326103a92b6547cceaced1e5bb745653abb1f324
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-07-06 21:00:40 +0200

    [BBoP] Odliczanie do zera - uzupełnienia.

--- bigbookpython/countdown.py
+++ bigbookpython/countdown.py
@@ -16,24 +16,26 @@ seconds_left = 30
 
 
 def main():
+    global seconds_left
     try:
         while True:
-            print('\n' * 60)
+            print('\n' * 2)
 
             hours = str(seconds_left // 3600)
-
             minutes = str((seconds_left % 3600) // 60)
-
             seconds = str(seconds_left % 60)
 
             h_digits = sevseg.get_sev_seg_str(hours, 2)
-            h_top_row, h_middle_row, h_bottom_row = h_digits.splitlines()
+            h2 = h_digits.splitlines()
+            h_top_row, h_middle_row, h_bottom_row = h2
 
             m_digits = sevseg.get_sev_seg_str(minutes, 2)
-            m_top_row, m_middle_row, m_bottom_row = m_digits.splitlines()
+            h3 = m_digits.splitlines()
+            m_top_row, m_middle_row, m_bottom_row = h3
 
             s_digits = sevseg.get_sev_seg_str(seconds, 2)
-            s_top_row, s_middle_row, s_bottom_row = s_digits.splitlines()
+            h4 = s_digits.splitlines()
+            s_top_row, s_middle_row, s_bottom_row = h4
 
             print(h_top_row    + "   " + m_top_row    + "   " + s_top_row)
             print(h_middle_row + " * " + m_middle_row + " * " + s_middle_row)
