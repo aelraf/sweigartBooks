3944bdcb4227253960bb353a9fa846e368c5a180
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2022-08-03 19:37:52 +0200

    [BBoSPP][carrotinbox] metody start(), print_boxes() i read_names()

--- bigbookpython/carrotinbox.py
+++ bigbookpython/carrotinbox.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # RafKac
+# na podstawie kodu udostępnionego przez Ala Sweigerta
 
 """
 Gra dla dwóch osób - marchew w pudełku.
@@ -9,3 +10,46 @@ Pierwszy gracz zagląda do swojego pudła i informuje drugiego, czy ma marchew.
 Drugi gracz decyduje, czy zamienić pudła, czy nie.
 """
 
+
+import random
+
+
+def start():
+    print("""
+    Gra dla dwóch ludzi. Każdy z nich ma pudło. W jednym pudle jest marchewka. 
+    Aby wygrać, musisz mieć pudło z marchewką.
+    
+    Pierwszy gracz patrzy do swojego pudła (drugi nie podgląda).
+    Pierwszy mówi "mam marchewkę" albo "nie mam". 
+    Wtedy drugi decyduje, czy zamienić pudła, czy nie. 
+    
+    """)
+    input('Press Enter to begin')
+
+
+def print_boxes(player_names, p1_name, p2_name):
+    print("""
+    Dwa pudła:
+      _______      _______
+     /      /|    /      /| 
+    +------+ |   +------+ |
+    | RED  | |   | GOLD | |
+    |      | /   |      | /
+    +------+/    +------+/
+    """)
+
+
+def read_names() -> (str, str):
+    p1_name = input("podaj imię pierwszego gracza: ")
+    p2_name = input("podaj imię drugiego gracza: ")
+    player_names = p1_name[:11].center(11) + "    " + p2_name[:11].center(11)
+    print_boxes(p1_name=p1_name, p2_name=p2_name, player_names=player_names)
+    print()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
